ccf02465d5f1cc6b43c3d07cb72be41a1a3a5155
Fernando <fermap83@gmail.com>
2021-02-20 16:04:12 +0100

    Level 7kyu - Vowels Kata

---
+++ src/main/java/com/codewars/kyu7/Vowels.java
@@ -0,0 +1,17 @@
+package com.codewars.kyu7;
+
+/*
+ * Return the number (count) of vowels in the given string.
+ * We will consider a, e, i, o, u as vowels for this Kata (but not y).
+ * The input string will only consist of lower case letters and/or spaces.
+ */
+public class Vowels {
+    public static int getCount(String str) {
+        int vowelsCount = 0;
+        for(char c : str.toCharArray()){
+            if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')
+                vowelsCount++;
+        }
+        return vowelsCount;
+    }
+}
